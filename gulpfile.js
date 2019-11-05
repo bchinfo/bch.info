8ef2dcc0103a9d05e47436b15247a5dea221c4a8
Corentin Mercier <corentin@mercier.link>
2019-11-05 13:04:04 +1000

    Added clean and html tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,29 @@
 let gulp = require('gulp'),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass')
+    del = require('del')
+    newer = require('gulp-newer');
 
 
-// Tasks
+// TASKS
+gulp.task('clean', function(done){
+  // Deletes all files from dist/
+  del.sync('dist/', {force: true});
+  done()
+})
+
 gulp.task('sass', function(){
   return gulp.src('scss/style.scss')
-    .pipe(sass())
+    .pipe(sass()) // Compiles styles.scss to css
     .pipe(gulp.dest('app/static/css'))
 })
+
+// Copy html files to dist
+gulp.task('html', function(){
+  return gulp.src('app/**/*.html')
+    .pipe(newer('dist/')) // Only get the modified files
+    .pipe(gulp.dest('dist/'))
+})
+
+
+// Default task
+gulp.task('default', gulp.series('clean', 'sass', 'html'))
