89cb3f37e9e91124cf01f87c8755fbf972c4cde8
Corentin Mercier <corentin@mercier.link>
2020-03-28 19:13:03 +0700

    Removed unused task

--- gulpfile.js
+++ gulpfile.js
@@ -37,14 +37,6 @@ gulp.task('copy-static', function(){
     .pipe(gulp.dest('dist/static/'));
 });
 
-gulp.task('browserSync', function(){
-  browserSync.init({
-    server: {
-      baseDir: 'app'
-    },
-  })
-});
-
 gulp.task('reload', function(done){
   reload();
   done();
