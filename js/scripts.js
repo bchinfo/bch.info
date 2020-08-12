02a349144d5649146ee932d81854be9060b1abff
Corentin Mercier <corentin@mercier.link>
2020-08-13 01:07:36 +0700

    Add custom menu viewport

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,16 @@ const moveTo = new MoveTo({easing: 'easeOutQuart'});
 
 const trigger = document.getElementsByClassName('js-trigger')[0];
 moveTo.registerTrigger(trigger);
+
+
+// Navigation menu toggler
+
+$("#menuToggler").click(function() { // open menu
+  $("#menu").show();
+  $("body").addClass('no-scrolling');
+});
+
+$("#closeMenu").click(function() { // close menu
+  $("#menu").hide();
+  $("body").removeClass('no-scrolling');
+});
