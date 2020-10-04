46e49561c90d860b2317ef8246180b3ab712175b
Corentin Mercier <corentin@mercier.link>
2020-10-04 15:42:44 +0700

    Refactor cashAddr conversion
    - Remove inline JavaScript
    - Load JavaScript at the end of the body
    - Add comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,8 @@
 // Custom scripts here
+
+
+// Initialize MoveTo
+
 const moveTo = new MoveTo({easing: 'easeOutQuart'});
 
 const trigger = document.getElementsByClassName('js-trigger')[0];
@@ -30,5 +34,39 @@ function copyToClipboard(element) {
     $(".btn-copy").removeClass("btn-primary").addClass("btn-secondary");
     $(".address-qrcode").removeClass("btn-primary");
     $(element).siblings().removeClass("btn-secondary").addClass("btn-primary");
+  }
+}
+
+
+// Address conversion
+// Warning: requires /static/js/bchaddrjs-0.4.9.min.js
+// and /static/js/qrcode.min.js
+
+function convertAddress() {
+  const address = $("#address").val();
+  if (address) {
+    if (bchaddr.isValidAddress(address)) {
+      $("#errorMessage").addClass("d-none");
+      // Conversion
+      const cash = bchaddr.toCashAddress(address);
+      const legacy = bchaddr.toLegacyAddress(address);
+      // Replace values
+      $("#cashAddr").text(cash);
+      $("#legacyAddr").text(legacy);
+      // Display converted address
+      $("#convertedAddr").removeClass("d-none");
+      // Display QR codes
+      QRCode.toCanvas(document.getElementById("cashAddrQR"), cash, function (error) {
+        if (error) console.error(error);
+      });
+      QRCode.toCanvas(document.getElementById("legacyAddrQR"), legacy, {width: 164}, function (error) {
+        if (error) console.error(error);
+      });
+    } else {
+      // Show error message
+      $("#convertedAddr").addClass("d-none");
+      $("#errorMessage").removeClass("d-none");
+      $("#errorMessage").text(address + " $( cashaddr.is-not-valid )$");
+    }
   }
 }
