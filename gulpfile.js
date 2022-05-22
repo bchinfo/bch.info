e162b9be40cd413b45b4f9de40973371dc8bb7ef
Corentin Mercier <corentin@mercier.link>
2022-05-22 13:24:24 +1000

    Replace node-sass with sass package
    As node-sass has compatibility issues with macOS.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 const gulp = require('gulp'),
-    sass = require('gulp-sass'),
+    sass = require('gulp-sass')(require('sass')),
     del = require('del'),
     browserSync = require('browser-sync'),
     cleanCSS = require('gulp-clean-css'),
